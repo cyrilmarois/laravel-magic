432f288e20f1da334228f61394de334c915c56f5
Cyril Marois <cyrilmarois@gmail.com>
2017-04-27 23:44:23 +0200

    Added Language's Module

---
+++ app/Modules/Language/Providers/ModuleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\Language\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'language');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'language');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations', 'language');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
