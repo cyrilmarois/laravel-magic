406b12dc125834db92a5e7962e08a90e29020e40
Cyril Marois <cyrilmarois@gmail.com>
2017-04-27 23:53:40 +0200

    Added Set's Module

---
+++ app/Modules/Set/Providers/ModuleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\Set\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'set');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'set');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations', 'set');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
