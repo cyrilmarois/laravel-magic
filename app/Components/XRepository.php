e9c824dd05fe9ac73bf390f4a012c8f446525928
Cyril Marois <cyrilmarois@gmail.com>
2017-07-18 01:12:02 +0200

    Added method to fetch all or one entity. Fix unique submit form data validation

--- app/Components/XRepository.php
+++ app/Components/XRepository.php
@@ -42,11 +42,36 @@ class XRepository
 		try {
 			Log::info('Trace in '.__METHOD__);
 
-			$model->validate($data, $model->rules['update']);
+			$data['id'] = $model->id;
+			$model->validate($data, 'update');
 			$model->fill($data);
 			$model->save();
 
 			return $model;
+		} catch (Exception $e) {
+			Log::error('Error in '.__METHOD__, ['message' => $e->getMessage()]);
+			throw $e;
+		}
+	}
+
+	public function all()
+	{
+		try {
+			Log::info('Trace in '.__METHOD__);
+
+			return ['data' => $this->_model::all()];
+		} catch (Exception $e) {
+			Log::error('Error in '.__METHOD__, ['message' => $e->getMessage()]);
+			throw $e;
+		}
+	}
+
+	public function one($id)
+	{
+		try {
+			Log::info('Trace in '.__METHOD__);
+
+			return $this->_model::find($id);
 		} catch (Exception $e) {
 			Log::error('Error in '.__METHOD__, ['message' => $e->getMessage()]);
 			throw $e;
