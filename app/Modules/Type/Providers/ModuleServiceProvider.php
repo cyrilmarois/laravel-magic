8935db131de91c81071a67a021b9ded15d6e563f
Cyril Marois <cyrilmarois@gmail.com>
2017-08-08 17:27:27 +0200

    Added Type's Module

---
+++ app/Modules/Type/Providers/ModuleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\Type\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'type');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'type');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations', 'type');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
