cfba0a29b9d8a471f1f646ae76587e3ad9988b48
Cyril Marois <cyrilmarois@gmail.com>
2017-08-09 11:49:36 +0200

    Added Artist's Module

---
+++ app/Modules/Artist/Providers/ModuleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\Artist\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'artist');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'artist');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations', 'artist');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
